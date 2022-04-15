9432e77cf5788de7be0b1b34654716757cf29b54
Valdemiro Viegas <valdemiroviegas@yahoo.com>
2022-04-15 09:37:39 +0100

    pagina de detalhes

--- Desktop/JavaScript/queen/pages/about.js
+++ Desktop/JavaScript/queen/pages/about.js
@@ -10,17 +10,11 @@ export default function About() {
     <div  className={styles.main}>
       <h1>Sobre o projeto</h1>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and 
-        typesetting industry. Lorem Ipsum has been  
+        Lorem Ipsum has been  
         standard dummy text ever since the 1500s, when an unknown 
         printer took a galley of type and scrambled it to make a 
         type specimen book. It has survived not only five centuries, 
-        but also the leap into electronic typesetting, remaining 
-        essentially unchangedLorem Ipsum is simply dummy text of 
-        the printing and typesetting industry. Lorem Ipsum has been 
-        the s standard dummy text ever since the 1500s, when 
-        an unknown printer took a galley of type and scrambled it to 
-        make a type specimen book. It has survived not only five centuries, 
+         It has survived not only five centuries, 
         but also the leap into electronic 
         typesetting, remaining essentially unchanged
       </p>
